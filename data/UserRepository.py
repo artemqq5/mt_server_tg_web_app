2caacb2eda0d06237e5c3ad4b3743b1bca75af0b
artem <artemqq5@gmail.com>
2024-06-21 12:14:41 +0300

    івв

--- data/UserRepository.py
+++ data/UserRepository.py
@@ -5,13 +5,25 @@ class UserRepository(DefaultDataBase):
     def __init__(self):
         super().__init__()
 
-    def add_user(self, user_id, username, firstname, lang_key, url):
-        query = "INSERT INTO `users` (user_id, username, firstname, lang_key, url) VALUES (%s, %s, %s, %s, %s);"
-        return self._insert(query, (user_id, username, firstname, lang_key, url))
+    # ALL USERS 
+
+    def add_user(self, user_id, username, firstname, lang_key):
+        query = "INSERT INTO `users` (user_id, username, firstname, lang_key) VALUES (%s, %s, %s, %s);"
+        return self._insert(query, (user_id, username, firstname, lang_key))
 
     def get_user(self, user_id):
         query = "SELECT * FROM `users` WHERE `user_id` = %s;"
-        return self._select_one(query, (user_id,))
+        return self._select_one(query, (user_id, ))
+    
+    # APP 1 - Gates Of Olympus
+    
+    def add_user_app1_olympus(self, user_id, username, firstname, lang_key, client_url):
+        query = "INSERT INTO `app1_olympus_users` (user_id, username, firstname, lang_key, url) VALUES (%s, %s, %s, %s, %s);"
+        return self._insert(query, (user_id, username, firstname, lang_key, client_url))
+
+    def get_user_app1_olympus(self, user_id):
+        query = "SELECT * FROM `app1_olympus_users` WHERE `user_id` = %s;"
+        return self._select_one(query, (user_id, ))
 
     # def get_users(self):
     #     query = "SELECT * FROM `users` WHERE `role` = %s;"
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import jwt
 import requests
 from flask import Flask, render_template, request, jsonify
 
-from config import BOT_TOKEN
+from config import APP1_OLYMPUS_BUNDLE, BOT_TOKEN_OLYMPUS_APP1
 from data.UserRepository import UserRepository
 
 app = Flask(__name__)
@@ -20,12 +20,12 @@ def home():
 def chekc_db():
     request_data = request.get_json()
     if not request_data:
-        return 400
+        return 'reqest is None', 400
     user = request_data.get("user", {})
     params = request_data.get("params", {})
 
-    if not user or not params:
-        return 400
+    if not user:
+        return 'User not exists', 400
 
     user_id = user.get("id", None)
     username = user.get("username", None)
@@ -34,27 +34,39 @@ def chekc_db():
     client_url = params.get("client_url", None)
     bundle = params.get("bundle", None)
 
-    if not UserRepository().get_user(user_id):
-        if not UserRepository().add_user(user_id, username, first_name, language_code, params):
-            return 400
-        else:
-            send_message(user_id, "Hello, Welcome to start game press 'Play' or /start", bundle)
-
     print(user)
     print(client_url)
     print(bundle)
+
+    if bundle == APP1_OLYMPUS_BUNDLE:
+        if not UserRepository().get_user_app1_olympus(user_id):
+            if not UserRepository().add_user_app1_olympus(user_id, username, first_name, language_code, client_url):
+                return 'Can`t add user to bot`s table', 400
+            else:
+                send_message(user_id, "Hello, Welcome to start game press 'Play' or /start", bundle)
+    else:
+        return 'Bot with bundle not exists', 400
+
+    if not UserRepository().get_user(user_id):
+        if not UserRepository().add_user(user_id, username, first_name, language_code):
+            return 'User can`t add to all users table', 400
 
     return 'OK', 200
 
 
 def send_message(chat_id, message, bundle):
-    url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
+    if bundle == APP1_OLYMPUS_BUNDLE:
+        bot_token = BOT_TOKEN_OLYMPUS_APP1
+    else:
+        return
+
+    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
     payload = {
         'chat_id': chat_id,
         'text': message
     }
-    response = requests.post(url, data=payload)
-    return response.json()
+
+    requests.post(url, data=payload)
 
 
 if __name__ == '__main__':
