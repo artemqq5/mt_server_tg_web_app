d7cbd9acc3cdf854bd74d364ad2142dbf1750f4e
artem <artemqq5@gmail.com>
2024-06-21 15:53:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,55 +18,59 @@ def home():
 
 @app.route('/check_db', methods=["POST"])
 def chekc_db():
-    request_data = request.get_json()
-    if not request_data:
-        print("reqest is None")
-        return 'reqest is None', 400
-    user = request_data.get("user", {})
-    params = request_data.get("params", {})
+    try:
+        request_data = request.get_json()
+        if not request_data:
+            print("reqest is None")
+            return 'reqest is None', 400
+        user = request_data.get("user", {})
+        params = request_data.get("params", {})
 
-    if not user:
-        print("User not exists")
-        return 'User not exists', 400
+        if not user:
+            print("User not exists")
+            return 'User not exists', 400
 
-    user_id = user.get("id", None)
-    username = user.get("username", None)
-    first_name = user.get("first_name", None)
-    language_code = user.get("language_code", None)
-    client_url = params.get("client_url", None)
-    bundle = params.get("bundle", None)
+        user_id = user.get("id", None)
+        username = user.get("username", None)
+        first_name = user.get("first_name", None)
+        language_code = user.get("language_code", None)
+        client_url = params.get("client_url", None)
+        bundle = params.get("bundle", None)
 
-    print("User:", remove_non_ascii(str(user)))
-    print("Client URL:", str(client_url))
-    print("Bundle:", str(bundle))
+        print("User:", remove_non_ascii(str(user)))
+        print("Client URL:", str(client_url))
+        print("Bundle:", str(bundle))
 
-    if bundle == APP1_OLYMPUS_BUNDLE:
-        if not UserRepository().get_user_app1_olympus(user_id):
-            if not UserRepository().add_user_app1_olympus(user_id, username, first_name, language_code, client_url):
-                print("Can`t add user to bot`s table")
-                return 'Can`t add user to bot`s table', 400
-            else:
-                send_message(user_id, "Hello, Welcome to start game press 'Play' or /start", bundle)
+        if bundle == APP1_OLYMPUS_BUNDLE:
+            if not UserRepository().get_user_app1_olympus(user_id):
+                if not UserRepository().add_user_app1_olympus(user_id, username, first_name, language_code, client_url):
+                    print("Can`t add user to bot`s table")
+                    return 'Can`t add user to bot`s table', 400
+                else:
+                    send_message(user_id, "Hello, Welcome to start game press 'Play' or /start", bundle)
 
-    elif bundle == APP2_JOKER_BUNDLE:
-        if not UserRepository().get_user_app2_joker(user_id):
-            if not UserRepository().add_user_app2_joker(user_id, username, first_name, language_code, client_url):
-                print("Can`t add user to bot`s table")
-                return 'Can`t add user to bot`s table', 400
-            else:
-                send_message(user_id, "Hello, Welcome to start game press 'Play' or /start", bundle)
-                
-    else:
-        print("Bot with bundle not exists")
-        return 'Bot with bundle not exists', 400
+        elif bundle == APP2_JOKER_BUNDLE:
+            if not UserRepository().get_user_app2_joker(user_id):
+                if not UserRepository().add_user_app2_joker(user_id, username, first_name, language_code, client_url):
+                    print("Can`t add user to bot`s table")
+                    return 'Can`t add user to bot`s table', 400
+                else:
+                    send_message(user_id, "Hello, Welcome to start game press 'Play' or /start", bundle)
 
-    if not UserRepository().get_user(user_id):
-        if not UserRepository().add_user(user_id, username, first_name, language_code):
-            print("User can`t add to all users table")
-            return 'User can`t add to all users table', 400
+        else:
+            print("Bot with bundle not exists")
+            return 'Bot with bundle not exists', 400
 
-    print("OK")
-    return 'OK', 200
+        if not UserRepository().get_user(user_id):
+            if not UserRepository().add_user(user_id, username, first_name, language_code):
+                print("User can`t add to all users table")
+                return 'User can`t add to all users table', 400
+
+        print("OK")
+        return 'OK', 200
+    except Exception as e:
+        print(e)
+        return 'Error', 400
 
 
 def send_message(chat_id, message, bundle):
